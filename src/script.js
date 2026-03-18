0fb71d42dd91483df1a8b567e1f6228ec7a3b70d
ivaaane <171681721+ivaaane@users.noreply.github.com>
2026-03-18 19:56:45 +0100

    owo

--- src/script.js
+++ src/script.js
@@ -1,6 +1,5 @@
 function changeSection(section) {
-    const sections = document.getElementsByClassName("select");
-    for (let i = 0; i < sections.length; i++)
-        document.getElementById(sections[i].value).style.display = "none";
-    document.getElementById(section.value).style.display = "block";
+    for (let sec of document.getElementsByClassName("section"))
+        sec.style.display = "none";
+    document.getElementById(section.id.replace("a-", "")).style.display = "block";
 }
