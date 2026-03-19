a8619ca3c9882bcf0a6b5a0a2cf78a05895e5853
ivaaane <171681721+ivaaane@users.noreply.github.com>
2026-03-19 16:49:28 +0100

    ods

--- src/script.js
+++ src/script.js
@@ -3,3 +3,10 @@ function changeSection(section) {
         sec.style.display = "none";
     document.getElementById(section.id.replace("a-", "")).style.display = "block";
 }
+
+function changeODS(ods) {
+    for (let sec of document.getElementsByClassName("ODS-desc"))
+        sec.style.display = "none";
+    for (let sec of document.getElementsByClassName(ods))
+        sec.style.display = "block";
+}
